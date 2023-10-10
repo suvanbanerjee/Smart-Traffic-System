e77b78e9056860f3d2800c0f7d2580ed34ce13e2
suvanbanerjee <banerjeesuvan@gmail.com>
2023-10-10 18:10:18 +0530

    updated linker to each signal updated main to include cases ppt minor brushes report minor changes

--- Code/linker.py
+++ Code/linker.py
@@ -4,24 +4,78 @@ import time
 board = pyfirmata.Arduino('COM4')
 it = pyfirmata.util.Iterator(board)
 it.start()
-red = board.get_pin('d:11:o')
-yellow = board.get_pin('d:12:o')
-green = board.get_pin('d:13:o')
+signal_1_red = board.get_pin('d:11:o')
+signal_1_yellow = board.get_pin('d:12:o')
+signal_1_green = board.get_pin('d:13:o')
+signal_2_red = board.get_pin('d:8:o')
+signal_2_yellow = board.get_pin('d:9:o')
+signal_2_green = board.get_pin('d:10:o')
+signal_3_red = board.get_pin('d:5:o')
+signal_3_yellow = board.get_pin('d:6:o')
+signal_3_green = board.get_pin('d:7:o')
+signal_4_red = board.get_pin('d:2:o')
+signal_4_yellow = board.get_pin('d:3:o')
+signal_4_green = board.get_pin('d:4:o')
 
-def RedToGreen(wait_yellow):
-    red.write(0)
-    yellow.write(1)
-    time.sleep(wait_yellow)
-    yellow.write(0)
-    green.write(1)
+def Signal_1_red_on():
+    signal_1_red.write(1)
+def Signal_1_red_off():
+    signal_1_red.write(0)
+def Signal_1_yellow_on():
+    signal_1_yellow.write(1)
+def Signal_1_yellow_off():
+    signal_1_yellow.write(0)
+def Signal_1_green_on():
+    signal_1_green.write(1)
+def Signal_1_green_off():
+    signal_1_green.write(0)
+def Signal_2_red_on():
+    signal_2_red.write(1)
+def Signal_2_red_off():
+    signal_2_red.write(0)
+def Signal_2_yellow_on():
+    signal_2_yellow.write(1)
+def Signal_2_yellow_off():
+    signal_2_yellow.write(0)
+def Signal_2_green_on():
+    signal_2_green.write(1)
+def Signal_2_green_off():
+    signal_2_green.write(0)
+def Signal_3_red_on():
+    signal_3_red.write(1)
+def Signal_3_red_off():
+    signal_3_red.write(0)
+def Signal_3_yellow_on():
+    signal_3_yellow.write(1)
+def Signal_3_yellow_off():
+    signal_3_yellow.write(0)
+def Signal_3_green_on():
+    signal_3_green.write(1)
+def Signal_3_green_off():
+    signal_3_green.write(0)
+def Signal_4_red_on():
+    signal_4_red.write(1)
+def Signal_4_red_off():
+    signal_4_red.write(0)
+def Signal_4_yellow_on():
+    signal_4_yellow.write(1)
+def Signal_4_yellow_off():
+    signal_4_yellow.write(0)
+def Signal_4_green_on():
+    signal_4_green.write(1)
+def Signal_4_green_off():
+    signal_4_green.write(0)
 
-def GreenToRed(wait_yellow):
-    green.write(0)
-    yellow.write(1)
-    time.sleep(wait_yellow)
-    yellow.write(0)
-    red.write(1)
-
-def main():
-    pass
-
+def All_off():
+    Signal_1_red_off()
+    Signal_1_yellow_off()
+    Signal_1_green_off()
+    Signal_2_red_off()
+    Signal_2_yellow_off()
+    Signal_2_green_off()
+    Signal_3_red_off()
+    Signal_3_yellow_off()
+    Signal_3_green_off()
+    Signal_4_red_off()
+    Signal_4_yellow_off()
+    Signal_4_green_off()
--- Code/main.py
+++ Code/main.py
@@ -3,6 +3,33 @@ import pandas as pd
 from ultralytics import YOLO
 from tracker import*
 import time
+from ftplib import FTP
+import linker
+
+
+# FTP server details
+ftp_host = '192.168.214.74'
+ftp_port = 2221
+ftp_user = 'admin'
+ftp_password = 'admin'
+
+#Connect to FTP server
+ftp = FTP()
+ftp.connect(ftp_host, ftp_port)
+ftp.login(ftp_user, ftp_password)
+
+
+def case_3():
+    pass
+
+def case_2_1():
+    signal_2.green(3)
+def case_2_2():
+    signal_1.green(3)
+
+def case_1():
+    if signal_1_density or signal_3_density > 1 or time_:
+
 
 model=YOLO('yolov8s.pt')
 def RGB(event, x, y, flags, param):
@@ -20,25 +47,36 @@ cy1=0
 cy2=0
 offset=6
 time_prev=time.time()
+
 while True: 
+
+
     with open('signal_1.txt', 'r') as signal1:
-         signal_1_density,signal_1_speed=map(int,str(signal1.read).split())
+         signal_1_density=str(signal1.read)
     with open('signal_2.txt', 'r') as signal2:
-         signal_2_density,signal_3_speed=map(int,str(signal2.read).split()) 
+         signal_2_density=str(signal2.read)
     with open('signal_1.txt', 'r') as signal3:
-         signal_3_density,signal_3_speed=map(int,str(signal3.read).split())  
+         signal_3_density=str(signal3.read)
+
 
     ret,frame = cap.read()
+
+
     if not ret:
         break
     count += 1
     if count % 3 != 0:
         continue
+
+
     frame=cv2.resize(frame,(360,480))
     results=model.predict(frame)
     a=results[0].boxes.boxes
     px=pd.DataFrame(a).astype("float")
-    list=[]    
+
+    list=[]
+
+
     for index,row in px.iterrows():
         x1=int(row[0])
         y1=int(row[1])
@@ -49,6 +87,8 @@ while True:
         if 'car' or 'bus' or 'motorcycle' or 'truck' in c:
             list.append([x1,y1,x2,y2])
         os.system('cls')
+
+
     bbox_id=tracker.update(list)
     for bbox in bbox_id:
         x3,y3,x4,y4,id=bbox
@@ -56,9 +96,59 @@ while True:
         cy=int((y3+y4)//2)+2
         cv2.circle(frame,(cx,cy),4,(0,0,255),-1)
         cv2.putText(frame,str("Vehicle"),(cx,cy),cv2.FONT_HERSHEY_COMPLEX,0.8,(0,255,255),2)
+    
+    time_delta = time.time() - time_prev
     print(len(list))
+
+    if time_delta > 30:
+
+        if (signal_1_density or signal_3_density > 20) and (signal_2_density or signal_4_density == 0 ):
+            linker.All_off()
+            linker.Signal_1_yellow_on()
+            linker.Signal_2_yellow_on()
+            linker.Signal_3_yellow_on()
+            linker.Signal_4_yellow_on()
+            time.sleep(3)
+            linker.Signal_1_yellow_off()
+            linker.Signal_2_yellow_off()
+            linker.Signal_3_yellow_off()
+            linker.Signal_4_yellow_off()
+
+            linker.Signal_1_green_on()
+            linker.Signal_2_red_on()
+            linker.Signal_3_green_on()
+            linker.Signal_4_red_on()
+
+        elif (signal_2_density or signal_4_density > 20) and (signal_1_density or signal_3_density == 0 ):
+            
+            linker.All_off()
+            linker.Signal_1_yellow_on()
+            linker.Signal_2_yellow_on()
+            linker.Signal_3_yellow_on()
+            linker.Signal_4_yellow_on()
+            time.sleep(3)
+            linker.Signal_1_yellow_off()
+            linker.Signal_2_yellow_off()
+            linker.Signal_3_yellow_off()
+            linker.Signal_4_yellow_off()
+        
+            linker.Signal_1_red_on()
+            linker.Signal_2_green_on()
+            linker.Signal_3_red_on()
+            linker.Signal_4_green_on()
+
+
+        elif (singal_2_density or signal_4_density < 2) and (signal_1_density or signal_3_density < 2 ):
+            
+            density based
+
+
+        else:
+            case_3()
+
     cv2.imshow("RGB", frame)
     if cv2.waitKey(1)&0xFF==27:
         break
+
 cap.release()
 cv2.destroyAllWindows()
